e2a519b23809d4d98e242515f7a07904bc5bbe18
haikhun <rogiatw@gmail.com>
2019-06-27 00:19:59 +0800

    fix home message

--- middleware/database.js
+++ middleware/database.js
@@ -4,9 +4,9 @@ class Database {
   constructor() {
     this._connect()
   }
-  
+
 _connect() {
-     mongoose.connect('mongodb://localhost:27017/software_final', { useNewUrlParser: true })
+    mongoose.connect('mongodb://140.119.164.35:27017/software_final', { useNewUrlParser: true })
        .then(() => {
          console.log('Database connection successful')
        })
